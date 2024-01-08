4d20da69d856ca2397d70161d1da86aa97deec9e
hafizruslan <145441557+hafizruslan@users.noreply.github.com>
2024-01-08 08:15:29 +0530

    add readonly

--- resources/views/pengurusan/penilai/view-profile.blade.php
+++ resources/views/pengurusan/penilai/view-profile.blade.php
@@ -4,62 +4,62 @@
 
              <div>
                 <label class="fw-bolder">Nama Pengguna/Penilai:</label>
-                <input type="text" class="form-control" name="nama_pengguna" value="{{$penilai->nama_pengguna}}">
+                <input type="text" class="form-control" name="nama_pengguna" value="{{$penilai->nama_pengguna}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder">No Kad Pengenalan:</label>
-                <input type="text" class="form-control" name="no_kad" value="{{$penilai->no_kad}}">
+                <input type="text" class="form-control" name="no_kad" value="{{$penilai->no_kad}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder"> Emel Peribadi:</label>
-                <input type="email" class="form-control" name="email_peribadi" value="{{$penilai->email_peribadi}}">
+                <input type="email" class="form-control" name="email_peribadi" value="{{$penilai->email_peribadi}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder"> Emel Ketua Jabatan:</label>
-                <input type="email" class="form-control" name="email_ketua_jabatan" value="{{$penilai->email_ketua_jabatan}}">
+                <input type="email" class="form-control" name="email_ketua_jabatan" value="{{$penilai->email_ketua_jabatan}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder"> Emel Penyelia:</label>
-                <input type="email" class="form-control" name="email_penyelia" value="{{$penilai->email_penyelia}}">
+                <input type="email" class="form-control" name="email_penyelia" value="{{$penilai->email_penyelia}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder"> Agensi/ Kementerian:</label>
-                <input type="text" class="form-control" name="agensi_kementerian" value="{{$penilai->agensi_kementerian}}">
+                <input type="text" class="form-control" name="agensi_kementerian" value="{{$penilai->agensi_kementerian}}" readonly>
             </div>
 
             <div>
                 <label class="fw-bolder"> No Tel Pejabat:</label>
-                <input type="text" class="form-control" name="no_tel_pejabat" value="{{$penilai->no_tel_pejabat}}">
+                <input type="text" class="form-control" name="no_tel_pejabat" value="{{$penilai->no_tel_pejabat}}" readonly>
             </div>
 
              <div>
                 <label class="fw-bolder"> No Tel Peribadi:</label>
-                <input type="text" class="form-control" name="no_tel_peribadi" value="{{$penilai->no_tel_peribadi}}">
+                <input type="text" class="form-control" name="no_tel_peribadi" value="{{$penilai->no_tel_peribadi}}" readonly>
             </div>
             
              <div>
                 <label class="fw-bolder"> Alamat 1:</label>
-                <input type="text" class="form-control" name="alamat1" value="{{$penilai->alamat1}}">
+                <input type="text" class="form-control" name="alamat1" value="{{$penilai->alamat1}}" readonly>
             </div>
 
              <div>
                 <label class="fw-bolder"> Alamat 2:</label>
-                <input type="text" class="form-control" name="alamat2" value="{{$penilai->alamat2}}">
+                <input type="text" class="form-control" name="alamat2" value="{{$penilai->alamat2}}" readonly>
             </div>
 
              <div>
                 <label class="fw-bolder"> Alamat 3:</label>
-                <input type="text" class="form-control" name="alamat3">
+                <input type="text" class="form-control" name="alamat3" value="{{$penilai->alamat3}}" readonly>
             </div>
 
              <div>
                 <label class="fw-bolder"> Poskod:</label>
-                <input type="text" class="form-control" maxlength="6" name="poskod" value="{{$penilai->poskod}}">
+                <input type="text" class="form-control" maxlength="6" name="poskod" value="{{$penilai->poskod}}" readonly>
             </div>
 
              <div>
