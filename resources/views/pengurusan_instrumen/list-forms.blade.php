728f110fd09f20832b6585eb1526a6da586016a0
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-08 17:00:51 +0800

    [Instrumen] Updated modal

--- resources/views/pengurusan_instrumen/list-forms.blade.php
+++ resources/views/pengurusan_instrumen/list-forms.blade.php
@@ -61,6 +61,20 @@ Pengurusan Instrumen
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div id="append">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('script')
