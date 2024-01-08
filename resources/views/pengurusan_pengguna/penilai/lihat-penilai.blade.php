773a5749b6118e82ef88035fd2fd9e2a9a129fd3
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-08 15:00:40 +0800

    [Maklumat Penilai] Updated UI

--- resources/views/pengurusan_pengguna/penilai/lihat-penilai.blade.php
+++ resources/views/pengurusan_pengguna/penilai/lihat-penilai.blade.php
@@ -1,106 +1,99 @@
-<div class="row invoice-add">
-    <div class="col-xl-12 col-md-12 col-12">
-        <div class="card invoice-preview-card">
+<div class="row">
+    <div class="col-md-9 mb-1">
+        <label class="form-label fw-bolder">Nama Pengguna/ Penilai</label>
+        <input type="text" class="form-control" name="nama_pengguna" value="{{$penilai->nama_pengguna}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder">Nama Pengguna/Penilai:</label>
-                <input type="text" class="form-control" name="nama_pengguna" value="{{$penilai->nama_pengguna}}" readonly>
-            </div>
+    <div class="col-md-3 mb-1">
+        <label class="form-label fw-bolder">No Kad Pengenalan/ Pasport</label>
+        <input type="text" class="form-control" name="no_kad" value="{{$penilai->no_kad}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder">No Kad Pengenalan:</label>
-                <input type="text" class="form-control" name="no_kad" value="{{$penilai->no_kad}}" readonly>
-            </div>
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bolder">Agensi/ Kementerian</label>
+        <input type="text" class="form-control" name="agensi_kementerian" value="{{$penilai->agensi_kementerian}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> Emel Peribadi:</label>
-                <input type="email" class="form-control" name="email_peribadi" value="{{$penilai->email_peribadi}}" readonly>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Emel Peribadi</label>
+        <input type="text" class="form-control" name="email_peribadi" value="{{$penilai->email_peribadi}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> Emel Ketua Jabatan:</label>
-                <input type="email" class="form-control" name="email_ketua_jabatan" value="{{$penilai->email_ketua_jabatan}}" readonly>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Emel Ketua Jabatan</label>
+        <input type="text" class="form-control" name="email_ketua_jabatan" value="{{$penilai->email_ketua_jabatan}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> Emel Penyelia:</label>
-                <input type="email" class="form-control" name="email_penyelia" value="{{$penilai->email_penyelia}}" readonly>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Emel Penyelia</label>
+        <input type="text" class="form-control" name="email_penyelia" value="{{$penilai->email_penyelia}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> Agensi/ Kementerian:</label>
-                <input type="text" class="form-control" name="agensi_kementerian" value="{{$penilai->agensi_kementerian}}" readonly>
-            </div>
+    <div class="col-md-6 mb-1">
+        <label class="form-label fw-bolder">No Tel Peribadi</label>
+        <input type="text" class="form-control" name="no_tel_peribadi" value="{{$penilai->no_tel_peribadi}}" disabled>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> No Tel Pejabat:</label>
-                <input type="text" class="form-control" name="no_tel_pejabat" value="{{$penilai->no_tel_pejabat}}" readonly>
-            </div>
+    <div class="col-md-6 mb-1">
+        <label class="form-label fw-bolder">No Tel Pejabat</label>
+        <input type="text" class="form-control" name="no_tel_pejabat" value="{{$penilai->no_tel_pejabat}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> No Tel Peribadi:</label>
-                <input type="text" class="form-control" name="no_tel_peribadi" value="{{$penilai->no_tel_peribadi}}" readonly>
-            </div>
-            
-             <div>
-                <label class="fw-bolder"> Alamat 1:</label>
-                <input type="text" class="form-control" name="alamat1" value="{{$penilai->alamat1}}" readonly>
-            </div>
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bolder">Alamat 1</label>
+        <input type="text" class="form-control" name="alamat1" value="{{$penilai->alamat1}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> Alamat 2:</label>
-                <input type="text" class="form-control" name="alamat2" value="{{$penilai->alamat2}}" readonly>
-            </div>
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bolder">Alamat 2</label>
+        <input type="text" class="form-control" name="alamat2" value="{{$penilai->alamat2}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> Alamat 3:</label>
-                <input type="text" class="form-control" name="alamat3" value="{{$penilai->alamat3}}" readonly>
-            </div>
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bolder">Alamat 3</label>
+        <input type="text" class="form-control" name="alamat3" value="{{$penilai->alamat3}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> Poskod:</label>
-                <input type="text" class="form-control" maxlength="6" name="poskod" value="{{$penilai->poskod}}" readonly>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Poskod</label>
+        <input type="text" class="form-control" name="poskod" value="{{$penilai->poskod}}" disabled>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> Daerah:</label>
-                  <select class="form-control select2" name="daerah" required>
-                        <option>select</option>
-                        <option>1</option>
-                        <option>2</option>
-                </select>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Daerah</label>
+        <select class="form-control select2" id="daerah" name="daerah" disabled>
+            <option value="" hidden>Gred</option>
+            <option value="1">Hulu Langat</option>
+            <option value="2">Ampang</option>
+        </select>
+    </div>
 
-             <div>
-                <label class="fw-bolder"> Negeri:</label>
-                  <select class="form-control select2" name="negeri" required>
-                        <option>select</option>
-                        @foreach($states as $state)
-                        <option value="{{$state->name}}" @if($state->name == $penilai->negeri) selected @endif>{{$state->name}}</option>
-                        @endforeach
-                </select>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Negeri</label>
+        <select class="form-control select2" id="negeri" name="negeri" disabled>
+            <option value="" hidden>Negeri</option>
+            @foreach($states as $state)
+                <option value="{{$state->name}}" @if($state->name == $penilai->negeri) selected @endif>{{$state->name}}</option>
+            @endforeach
+        </select>
+    </div>
 
-            <div>
-                <label class="fw-bolder"> Gred:</label>
-                  <select class="form-control select2" name="gred">
-                        <option>select</option>
-                        <option>1</option>
-                        <option>2</option>
-                </select>
-            </div>
+    <div class="col-md-4 mb-1">
+        <label class="form-label fw-bolder">Gred</label>
+        <select class="form-control select2" id="gred" name="gred" disabled>
+            <option value="" hidden>Gred</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+        </select>
+    </div>
 
-
-            <div>
-                <label class="fw-bolder"> 3 negeri pilihan bagi menjalankan penilaian SKPAK:</label>
-                  <select class="form-control select2" name="negeri_skpak[]" multiple>
-                        <option>select</option>
-                        @foreach($states as $state)
-                            <option value="{{$state->name}}">{{$state->name}}</option>
-                        @endforeach
-                </select>
-            </div>
-
-        </div>
+    <div class="col-md-8 mb-1">
+        <label class="form-label fw-bolder">3 Negeri Pilihan Bagi Menjalankan Penilaian SKPAK</label>
+        <select class="form-control select2" id="negeri_skpak[]" name="negeri_skpak[]" multiple disabled>
+            <option value="" hidden>Negeri</option>
+            @foreach($states as $state)
+                <option value="{{$state->name}}">{{$state->name}}</option>
+            @endforeach
+        </select>
     </div>
 </div>
