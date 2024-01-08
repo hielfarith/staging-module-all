9692262c65dfaf8657e5a6fa3412250f0fd58b64
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-08 17:09:52 +0800

    [Telah dijawab] Updated UI

--- resources/views/pengurusan_instrumen/instrumen_dijawab/senarai_telah_dijawab.blade.php
+++ resources/views/pengurusan_instrumen/instrumen_dijawab/senarai_telah_dijawab.blade.php
@@ -3,11 +3,7 @@
 @section('header')
 Pengurusan Instrumen
 @endsection
-<style type="text/css">
-    .delete-button {
-        display: none;
-    }    
-</style>
+
 @section('breadcrumb')
 <li class="breadcrumb-item">
     <a href="{{ route('home') }}">{{ __('msg.home') }}</a>
@@ -19,6 +15,20 @@ Pengurusan Instrumen
 @endsection
 
 @section('content')
+<style>
+    #TableSenaraiInstrumenDijawab thead th {
+        vertical-align: middle;
+        text-align: center;
+    }
+
+    #TableSenaraiInstrumenDijawab tbody {
+        vertical-align: middle;
+    }
+
+    .delete-button {
+        display: none;
+    }
+</style>
 <div class="card">
     <div class="card-header">
         <h4 class="card-title fw-bolder"> Senarai Instrumen Telah Dijawab </h4>
@@ -34,31 +44,47 @@ Pengurusan Instrumen
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table header_uppercase table-bordered table-hovered" id="TableSenaraiInstrumen">
+            <table class="table header_uppercase table-bordered table-hovered" id="TableSenaraiInstrumenDijawab" >
+                {{-- <thead>
+                    <tr>
+                        <th width="5%">No.</th>
+                        <th>Nama Instrumen</th>
+                        <th>Kategori Instrumen</th>
+                        <th>Jumlah Pengisian</th>
+                        <th width="5%">Tindakan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody> --}}
+
                 <thead>
                     <tr>
                         <th width="5%">No.</th>
                         <th>Nama Instrumen</th>
                         <th>Kategori Instrumen</th>
-                        <th width="20%">Jumlah Pengisian</th>
+                        <th>Jumlah Pengisian</th>
                         <th width="5%">Tindakan</th>
                     </tr>
-                </thead>
-                <tbody>
-                </tbody>
+                  </thead>
+
+                  <tbody>
+
+                  </tbody>
             </table>
         </div>
     </div>
 </div>
 
-<div class="modal fade text-start" id="modal-instrumen-diisi" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+<div class="modal fade" id="modal-instrumen-diisi" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Maklumat Pengisian Instrumen</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body" id="modal-body-instrumen-diisi"></div>
+            <div class="modal-body">
+                <div id="modal-body-instrumen-diisi">
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -66,95 +92,96 @@ Pengurusan Instrumen
 
 @section('script')
 <script>
-$(document).ready(function() {
-    $(function() {
-        var table = $('#TableSenaraiInstrumen').DataTable({
-            orderCellsTop: true,
-            colReorder: false,
-            pageLength: 10,
-            processing: true,
-            serverSide: true, //enable if data is large (more than 50,000)
-            ajax: {
-                url: "{{ fullUrl() }}",
-                cache: false,
-            },
-            columns: [{
-                    data: "id",
-                    name: "id",
-                    render: function(data, type, row) {
-                        return $("<div/>").html(data).text();
-                    }
+    $(document).ready(function() {
+
+        $(function() {
+            var table = $('#TableSenaraiInstrumenDijawab').DataTable({
+                orderCellsTop: true,
+                colReorder: false,
+                pageLength: 10,
+                processing: true,
+                serverSide: true, //enable if data is large (more than 50,000)
+                ajax: {
+                    url: "{{ fullUrl() }}",
+                    cache: false,
                 },
-                {
-                    data: "form_name",
-                    name: "form_name",
-                    render: function(data, type, row) {
-                        return $("<div/>").html(data).text();
-                    }
-                },
-                {
-                    data: "category",
-                    name: "category",
-                    render: function(data, type, row) {
-                        return $("<div/>").html(data).text();
-                    }
-                },
-                {
-                    data: "submission_count",
-                    name: "submission_count",
-                    render: function(data, type, row) {
-                        return $("<div/>").html(data).text();
-                    }
-                },
-                {
-                    data: 'action',
-                    name: 'action',
-                    orderable: true,
-                    searchable: true
-                },
-            ],
+                columns: [{
+                        data: "id",
+                        name: "id",
+                        render: function(data, type, row) {
+                            return $("<div/>").html(data).text();
+                        }
+                    },
+                    {
+                        data: "form_name",
+                        name: "form_name",
+                        render: function(data, type, row) {
+                            return $("<div/>").html(data).text();
+                        }
+                    },
+                    {
+                        data: "category",
+                        name: "category",
+                        render: function(data, type, row) {
+                            return $("<div/>").html(data).text();
+                        }
+                    },
+                    {
+                        data: "submission_count",
+                        name: "submission_count",
+                        render: function(data, type, row) {
+                            return $("<div/>").html(data).text();
+                        }
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: true,
+                        searchable: true
+                    },
+                ],
+            });
         });
     });
-});
 
-$.ajaxSetup({
+    $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
-})
+    })
 
-function maklumatPengisianInstrumen(id){
-    var url = "{{ route('instrumen_dijawab',['id'=> ':id']) }}";
-    var url = url.replace(':id', id);
+    function maklumatPengisianInstrumen(id){
+        var url = "{{ route('instrumen_dijawab',['id'=> ':id']) }}";
+        var url = url.replace(':id', id);
 
-    $.ajax({
-        url: url, // Route URL
-        type: 'POST', // Request type (GET, POST, etc.)
-            data: {
-            id: id
-            },
-        success: function(response) {
-            $('#modal-instrumen-diisi').modal("show");
-            $('#modal-body-instrumen-diisi').empty();
-            $('#modal-body-instrumen-diisi').append(response);
-        }
-    });
-}
+        $.ajax({
+            url: url, // Route URL
+            type: 'POST', // Request type (GET, POST, etc.)
+                data: {
+                id: id
+                },
+            success: function(response) {
+                $('#modal-instrumen-diisi').modal("show");
+                $('#modal-body-instrumen-diisi').empty();
+                $('#modal-body-instrumen-diisi').append(response);
+            }
+        });
+    }
 
-function  formverify(status, formid) {
+    function  formverify(status, formid) {
         var url = "{{route('verify')}}";
 
         $.ajax({
             url: url, // Route URL
             type: 'POST', // Request type (GET, POST, etc.)
-             data: {
+            data: {
                 status: status,
                 formid: formid
-             }, 
+            },
             success: function(response) {
                 if (response.success) {
                     window.location.reload();
-               } 
+            }
             }
         });
     }
