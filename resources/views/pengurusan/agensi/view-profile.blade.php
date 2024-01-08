f09839724df814161a9d1837b9ab8038582dfd7e
hafizruslan <145441557+hafizruslan@users.noreply.github.com>
2024-01-08 09:56:09 +0530

    add view frm

--- resources/views/pengurusan/agensi/view-profile.blade.php
+++ resources/views/pengurusan/agensi/view-profile.blade.php
@@ -4,81 +4,41 @@
 
              <div>
                 <label class="fw-bolder">Nama Pengguna/Penilai:</label>
-                <input type="text" class="form-control" name="nama_pengguna" value="{{$penilai->nama_pengguna}}" readonly>
+                <input type="text" class="form-control" name="nama_pengguna">
+            </div>
+
+            <div>
+                <label class="fw-bolder"> Panggilan:</label>
+                  <select class="form-control select2" name="panggilan">
+                        <option>select</option>
+                        <option value="Dato'">Dato'</option>
+                        <option value="Datin">Datin</option>
+                        <option value="Prof.">Prof.</option>
+                        <option value="Prof Madya">Prof Madya</option>
+                        <option value="Dr.">Dr.</option>
+                        <option value="Tuan">Tuan</option>
+                        <option value="Puan">Puan</option>
+                        <option value="Cik">Cik</option>
+                </select>
             </div>
 
             <div>
                 <label class="fw-bolder">No Kad Pengenalan:</label>
-                <input type="text" class="form-control" name="no_kad" value="{{$penilai->no_kad}}" readonly>
+                <input type="text" class="form-control" name="no_kad">
             </div>
 
             <div>
-                <label class="fw-bolder"> Emel Peribadi:</label>
-                <input type="email" class="form-control" name="email_peribadi" value="{{$penilai->email_peribadi}}" readonly>
+                <label class="fw-bolder">
+                    <input type="radio" name="jenis" class="form-check-input" value="Kerajan" disabled>Kerajan
+                </label>
+                <label class="fw-bolder">
+                    <input type="radio" name="jenis"class="form-check-input"  value="Swasta" disabled>Swasta
+                </label>
             </div>
 
             <div>
-                <label class="fw-bolder"> Emel Ketua Jabatan:</label>
-                <input type="email" class="form-control" name="email_ketua_jabatan" value="{{$penilai->email_ketua_jabatan}}" readonly>
-            </div>
-
-            <div>
-                <label class="fw-bolder"> Emel Penyelia:</label>
-                <input type="email" class="form-control" name="email_penyelia" value="{{$penilai->email_penyelia}}" readonly>
-            </div>
-
-            <div>
-                <label class="fw-bolder"> Agensi/ Kementerian:</label>
-                <input type="text" class="form-control" name="agensi_kementerian" value="{{$penilai->agensi_kementerian}}" readonly>
-            </div>
-
-            <div>
-                <label class="fw-bolder"> No Tel Pejabat:</label>
-                <input type="text" class="form-control" name="no_tel_pejabat" value="{{$penilai->no_tel_pejabat}}" readonly>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> No Tel Peribadi:</label>
-                <input type="text" class="form-control" name="no_tel_peribadi" value="{{$penilai->no_tel_peribadi}}" readonly>
-            </div>
-            
-             <div>
-                <label class="fw-bolder"> Alamat 1:</label>
-                <input type="text" class="form-control" name="alamat1" value="{{$penilai->alamat1}}" readonly>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> Alamat 2:</label>
-                <input type="text" class="form-control" name="alamat2" value="{{$penilai->alamat2}}" readonly>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> Alamat 3:</label>
-                <input type="text" class="form-control" name="alamat3" value="{{$penilai->alamat3}}" readonly>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> Poskod:</label>
-                <input type="text" class="form-control" maxlength="6" name="poskod" value="{{$penilai->poskod}}" readonly>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> Daerah:</label>
-                  <select class="form-control select2" name="daerah" required>
-                        <option>select</option>
-                        <option>1</option>
-                        <option>2</option>
-                </select>
-            </div>
-
-             <div>
-                <label class="fw-bolder"> Negeri:</label>
-                  <select class="form-control select2" name="negeri" required>
-                        <option>select</option>
-                        @foreach($states as $state)
-                        <option value="{{$state->name}}" @if($state->name == $penilai->negeri) selected @endif>{{$state->name}}</option>
-                        @endforeach
-                </select>
+                <label class="fw-bolder">Jawatan:</label>
+                <input type="text" name="jawatan" value="" class="form-control">
             </div>
 
             <div>
@@ -90,15 +50,75 @@
                 </select>
             </div>
 
-
             <div>
-                <label class="fw-bolder"> 3 negeri pilihan bagi menjalankan penilaian SKPAK:</label>
-                  <select class="form-control select2" name="negeri_skpak[]" multiple>
+                <label class="fw-bolder">
+                    <input type="checkbox" name="modul" value="1" class="form-check-input" disabled>Modul
+                </label>
+            </div>
+           
+            <div>
+                <label class="fw-bolder"> Agensi/ Kementerian:</label>
+                  <select class="form-control select2" name="agensi_kementerian[]" multiple>
+                        <option>select</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                </select>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Alamat 1:</label>
+                <input type="text" class="form-control" name="alamat1" readonly>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Alamat 2:</label>
+                <input type="text" class="form-control" name="alamat2" readonly>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Alamat 3:</label>
+                <input type="text" class="form-control" name="alamat3" readonly>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Poskod:</label>
+                <input type="text" class="form-control" maxlength="5" name="poskod" readonly>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Daerah:</label>
+                  <select class="form-control select2" name="daerah" readonly>
+                        <option>select</option>
+                        <option>1</option>
+                        <option>2</option>
+                </select>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> Negeri:</label>
+                  <select class="form-control select2" name="negeri" readonly>
                         <option>select</option>
                         @foreach($states as $state)
-                            <option value="{{$state->name}}">{{$state->name}}</option>
+                        <option value="{{$state->name}}">{{$state->name}}</option>
                         @endforeach
                 </select>
+            </div>
+
+            <div>
+                <label class="fw-bolder">  Emel Ketua Pengarah/ Pengarah:</label>
+                <input type="email" class="form-control" name="email_ketua" readonly>
+            </div>
+            
+            <div>
+                <label class="fw-bolder"> No Tel Pejabat:</label>
+                <input type="text" class="form-control" name="no_tel_pejabat" readonly>
+            </div>
+
+             <div>
+                <label class="fw-bolder"> No Tel Peribadi:</label>
+                <input type="text" class="form-control" name="no_tel_peribadi" readonly>
             </div>
 
         </div>
