d93b61db32ce57fc7a4101b2b26ba6b8e9c8a539
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-09 17:30:39 +0800

    [Ketua Agensi] Updated UI

--- resources/views/pengurusan/agensi/form.blade.php
+++ resources/views/pengurusan/agensi/form.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
+
 @section('header')
-Pengurusan KETUA AGENSI
+Pengurusan Ketua Agensi
 @endsection
 
 @section('breadcrumb')
@@ -9,12 +10,198 @@ Pengurusan KETUA AGENSI
 </li>
 
 <li class="breadcrumb-item">
-    <a href="#"> Pengurusan KETUA AGENSI </a>
+    <a href="#"> Pengurusan Pengguna </a>
+</li>
+
+<li class="breadcrumb-item">
+    <a href="#"> Maklumat Ketua Agensi Baru </a>
 </li>
 @endsection
 
 @section('content')
-<div class="row">
+<style>
+    .delete-button {
+        display: none;
+    }
+</style>
+
+<div class="card">
+    <div class="card-body">
+        <form id="formagensi">
+            <div class="row">
+                <h5 class="mb-2 fw-bold">
+                    <span class="badge rounded-pill badge-light-primary">
+                        Maklumat Ketua Agensi
+                    </span>
+                </h5>
+
+                <div class="col-sm-9 col-12 mb-1">
+                    <label class="form-label fw-bolder">Nama Ketua Agensi/ Pengguna</label>
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <select class="form-control select2" name="panggilan" required>
+                                <option value="" hidden>Gelaran</option>
+                                <option value="Dato'">Dato'</option>
+                                <option value="Datin">Datin</option>
+                                <option value="Prof.">Prof.</option>
+                                <option value="Prof Madya">Prof Madya</option>
+                                <option value="Dr.">Dr.</option>
+                                <option value="Tuan">Tuan</option>
+                                <option value="Puan">Puan</option>
+                                <option value="Cik">Cik</option>
+                            </select>
+                        </span>
+                        <input type="text" class="form-control" name="nama_pengguna" required onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)">
+                    </div>
+                </div>
+
+                <div class="col-md-3 mb-1">
+                    <label class="fw-bold form-label">No Kad Pengenalan/ Pasport
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="text" class="form-control" name="no_kad" required>
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">Emel Ketua Pengarah/ Pengarah
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="email" class="form-control" name="email_ketua" required>
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">No Tel Pejabat
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="text" class="form-control" name="no_tel_pejabat" maxlength="12" onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">No Tel Peribadi
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="text" class="form-control" name="no_tel_peribadi" maxlength="12" required onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+                </div>
+
+                <div class="col-md-3 mb-1">
+                    <label class="fw-bold form-label">Jenis
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="jenis" required>
+                        <option value="" hidden>Jenis</option>
+                        <option value="Kerajaan">Kerajaan</option>
+                        <option value="Swasta">Swasta</option>
+                    </select>
+                </div>
+
+                <div class="col-md-3 mb-1">
+                    <label class="fw-bold form-label">Jawatan
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="jawatan" id="jawatan" required>
+                            <option value="" hidden>Jawatan</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                    </select>
+                </div>
+
+                <div class="col-md-2 mb-1">
+                    <label class="fw-bold form-label">Gred
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="gred" id="gred" required>
+                        <option value="" hidden>Gred</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+
+                <div class="col-md-1 mb-1">
+                    <div class="form-check form-check-inline mt-2">
+                        <input class="form-check-input" type="checkbox" id="modul" name="modul" value="1" required/>
+                        <label class="form-check-label fw-bolder">Modul
+                            <span class="text-danger">*</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-md-3 mb-1">
+                    <label class="fw-bold form-label">Agensi/ Kementerian
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="agensi_kementerian[]" multiple required>
+                        <option value="" hidden>Agensi/ Kementerian</option>
+                        <option value="Agensi">Agensi</option>
+                        <option value="Kementerian">Kementerian</option>
+                    </select>
+                </div>
+
+                <hr>
+                <h5 class="mb-2 fw-bold">
+                    <span class="badge rounded-pill badge-light-primary">
+                        Maklumat Alamat
+                    </span>
+                </h5>
+
+                <div class="col-md-12 mb-1">
+                    <label class="fw-bold form-label">Alamat
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="text" class="form-control" name="alamat1" placeholder="Alamat 1" required>
+                </div>
+
+                <div class="col-md-12 mb-1">
+                    {{-- <label class="fw-bold form-label">Alamat 2
+                        <span class="text-danger">*</span>
+                    </label> --}}
+                    <input type="text" class="form-control" name="alamat2" placeholder="Alamat 2" required>
+                </div>
+
+                <div class="col-md-12 mb-1">
+                    {{-- <label class="fw-bold form-label">Alamat 3</label> --}}
+                    <input type="text" class="form-control" name="alamat3" placeholder="Alamat 3">
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">Negeri
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="negeri" required>
+                        <option value="" hidden>Negeri</option>
+                        @foreach($states as $state)
+                            <option value="{{$state->name}}">{{$state->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">Daerah
+                        <span class="text-danger">*</span>
+                    </label>
+                    <select class="form-control select2" name="daerah" required>
+                        <option value="" hidden>Daerah</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+
+                <div class="col-md-4 mb-1">
+                    <label class="fw-bold form-label">Poskod
+                        <span class="text-danger">*</span>
+                    </label>
+                    <input type="text" class="form-control" name="poskod" maxlength="5" required onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+                </div>
+            </div>
+
+            <hr>
+            <div class="d-flex justify-content-end align-items-center mt-1">
+                <button type="submit" class="btn btn-primary float-right">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+{{-- <div class="row">
     <div class="col-md-8">
         <form id="formagensi">
             <div>
@@ -42,22 +229,13 @@ Pengurusan KETUA AGENSI
                 <input type="text" class="form-control" name="no_kad" required>
             </div>
 
-           <!--  <div>
-                <label class="fw-bolder">
-                    <input type="radio" name="jenis" class="form-check-input" value="Kerajan">Kerajan
-                </label>
-                <label class="fw-bolder">
-                    <input type="radio" name="jenis"class="form-check-input"  value="Swasta">Swasta
-                </label>
-            </div> -->
-
              <div>
                 <label class="fw-bolder"> Jenis :<span style="color: red;">*</span></label>
                 <select class="form-control select2" name="jenis" required>
                         <option>pilih</option>
                         <option value="Kerajaan">Kerajaan</option>
                         <option value="Swasta">Swasta</option>
-                </select> 
+                </select>
             </div>
 
             <div>
@@ -79,7 +257,7 @@ Pengurusan KETUA AGENSI
                     <input type="checkbox" name="modul" value="1" class="form-check-input" required>Modul
                 </label>
             </div>
-           
+
             <div>
                 <label class="fw-bolder"> Agensi/ Kementerian:</label>
                   <select class="form-control select2" name="agensi_kementerian[]" multiple required>
@@ -132,7 +310,7 @@ Pengurusan KETUA AGENSI
                 <label class="fw-bolder">  Emel Ketua Pengarah/ Pengarah:</label>
                 <input type="email" class="form-control" name="email_ketua" required>
             </div>
-            
+
             <div>
                 <label class="fw-bolder"> No Tel Pejabat:</label>
                 <input type="text" class="form-control" name="no_tel_pejabat" maxlength="12" onkeypress='return event.charCode >= 48 && event.charCode <= 57' >
@@ -145,16 +323,16 @@ Pengurusan KETUA AGENSI
 
             <div class="d-flex justify-content-end align-items-center my-1">
                 <button type="submit" class="btn btn-primary float-right">Hantar</button>
-            </div>    
+            </div>
         </form>
     </div>
-</div>
+</div> --}}
 
 @endsection
 
 @section('script')
 <script type="text/javascript">
-    
+
 $('#formagensi').submit(function(event) {
         event.preventDefault();
         var formData = new FormData(document.getElementById('formagensi'));
--- resources/views/pengurusan/agensi/list.blade.php
+++ resources/views/pengurusan/agensi/list.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('header')
-Pengurusan KETUA AGENSI
+Pengurusan Ketua Agensi
 @endsection
 
 @section('breadcrumb')
@@ -10,14 +10,18 @@ Pengurusan KETUA AGENSI
 </li>
 
 <li class="breadcrumb-item">
-    <a href="#"> Pengurusan KETUA AGENSI </a>
+    <a href="#"> Pengurusan Pengguna </a>
+</li>
+
+<li class="breadcrumb-item">
+    <a href="#"> Pengurusan Ketua Agensi </a>
 </li>
 @endsection
 
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h4 class="card-title fw-bolder"> Senarai Penggurusan Ketua Agensi </h4>
+        <h4 class="card-title fw-bolder"> Senarai Ketua Agensi </h4>
 
         <div class="d-flex justify-content-end align-items-center">
             <a type="button" class="btn btn-primary float-right" href="{{ route('admin.internal.agensiform') }}">
@@ -25,18 +29,46 @@ Pengurusan KETUA AGENSI
             </a>
         </div>
     </div>
- 
+
     <hr>
 
     <div class="card-body">
+        <div class="row">
+            <div class="col-md-4">
+                <label class="form-label">Nama Ketua Agensi/ Pengguna</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="col-md-4">
+                <label class="form-label">No. Kad Pengenalan/ Pasport</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="col-md-4">
+                <label class="form-label">Emel Ketua Pengarah/ Pengarah</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end align-items-center mt-1">
+            <a class="me-3 text-danger" type="button" id="reset" href="#">
+                Setkan Semula
+            </a>
+            <button type="submit" value="Filter" class="btn btn-success float-right">
+                <i class="fa fa-search me-1"></i> Cari
+            </button>
+        </div>
+
+        <hr>
+
         <div class="table-responsive">
             <table class="table header_uppercase table-bordered table-hovered" id="TableSenaraiAgensi">
                 <thead>
                     <tr>
                         <th width="5%">No.</th>
-                        <th>Nama Pengguna</th>
-                        <th>No Kad</th>
-                        <th width="20%">Emel Ketua Pengarah/ Pengarah</th>
+                        <th>Nama Ketua Agensi/ Pengguna</th>
+                        <th>No. Kad Pengenalan/ Pasport</th>
+                        <th>Emel Ketua Pengarah/ Pengarah</th>
                         <th width="5%">Tindakan</th>
                     </tr>
                 </thead>
@@ -47,14 +79,16 @@ Pengurusan KETUA AGENSI
     </div>
 </div>
 
-<div class="modal fade text-start" id="modal-agensi-diisi" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+<div class="modal fade" id="modal-agensi-diisi" tabindex="-1" aria-labelledby="modal-agensi-diisi" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Maklumat Penilai</h4>
+                <h5 class="modal-title" id="exampleModalScrollableTitle">Maklumat Ketua Agensi/ Pengguna</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body" id="modal-body-agensi"></div>
+
+            <div class="modal-body" id="modal-body-agensi">
+            </div>
         </div>
     </div>
 </div>
@@ -150,11 +184,11 @@ function  formverify(status, formid) {
              data: {
                 status: status,
                 formid: formid
-             }, 
+             },
             success: function(response) {
                 if (response.success) {
                     window.location.reload();
-               } 
+               }
             }
         });
     }
