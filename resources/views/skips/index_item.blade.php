8fbab0d678c857478b17904fdb0e96be697c1a61
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-19 12:14:01 +0800

    [SKIPS] New module

---
+++ resources/views/skips/index_item.blade.php
@@ -0,0 +1,85 @@
+<ul class="nav nav-pills nav-justified" role="tablist">
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder active" id="item-1-tab" data-bs-toggle="tab" href="#item-1" aria-controls="item-1" role="tab" aria-selected="true">
+            1.0 PENUBUHAN & PENDAFTARAN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-2-tab" data-bs-toggle="tab" href="#item-2" aria-controls="item-2" role="tab" aria-selected="true">
+            2.0 PENGURUSAN INSTITUSI
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-3-tab" data-bs-toggle="tab" href="#item-3" aria-controls="item-3" role="tab" aria-selected="true">
+            3.0 PENGURUSAN KURIKULUM
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-4-tab" data-bs-toggle="tab" href="#item-4" aria-controls="item-4" role="tab" aria-selected="true">
+            4.0 PENGAJARAN DAN PEMBELAJARAN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-5-tab" data-bs-toggle="tab" href="#item-5" aria-controls="item-5" role="tab" aria-selected="true">
+            5.0 PENGURUSAN PENILAIAN / PEPERIKSAAN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-6-tab" data-bs-toggle="tab" href="#item-6" aria-controls="item-6" role="tab" aria-selected="true">
+            6.0 PENGURUSAN DAN PEMBANGUNAN GURU
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-7-tab" data-bs-toggle="tab" href="#item-7" aria-controls="item-7" role="tab" aria-selected="true">
+            7.0 DISIPLIN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-8-tab" data-bs-toggle="tab" href="#item-8" aria-controls="item-8" role="tab" aria-selected="true">
+            8.0 PIAWAIAN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-9-tab" data-bs-toggle="tab" href="#item-9" aria-controls="item-9" role="tab" aria-selected="true">
+            9.0 KEBERSIHAN DAN KECERIAAN
+        </a>
+    </li>
+    <li class="nav-item" role="presentation">
+        <a class="text-uppercase text-wrap nav-link fw-bolder" id="item-10-tab" data-bs-toggle="tab" href="#item-10" aria-controls="item-10" role="tab" aria-selected="true">
+            10.0 PENGURUSAN PELAJAR ANTARABANGSA
+        </a>
+    </li>
+</ul>
+
+<div class="tab-content">
+    <div class="tab-pane fade show active" id="item-1" role="tabpanel" aria-labelledby="item-1-tab">
+        @include('skips.borang_skips.item_penilaian.item_1')
+    </div>
+    <div class="tab-pane fade" id="item-2" role="tabpanel" aria-labelledby="item-2-tab">
+        @include('skips.borang_skips.item_penilaian.item_2')
+    </div>
+    <div class="tab-pane fade" id="item-3" role="tabpanel" aria-labelledby="item-3-tab">
+        @include('skips.borang_skips.item_penilaian.item_3')
+    </div>
+    <div class="tab-pane fade" id="item-4" role="tabpanel" aria-labelledby="item-4-tab">
+        @include('skips.borang_skips.item_penilaian.item_4')
+    </div>
+    <div class="tab-pane fade" id="item-5" role="tabpanel" aria-labelledby="item-5-tab">
+        @include('skips.borang_skips.item_penilaian.item_5')
+    </div>
+    <div class="tab-pane fade" id="item-6" role="tabpanel" aria-labelledby="item-6-tab">
+        @include('skips.borang_skips.item_penilaian.item_6')
+    </div>
+    <div class="tab-pane fade" id="item-7" role="tabpanel" aria-labelledby="item-7-tab">
+        @include('skips.borang_skips.item_penilaian.item_7')
+    </div>
+    <div class="tab-pane fade" id="item-8" role="tabpanel" aria-labelledby="item-8-tab">
+        @include('skips.borang_skips.item_penilaian.item_8')
+    </div>
+    <div class="tab-pane fade" id="item-9" role="tabpanel" aria-labelledby="item-9-tab">
+        @include('skips.borang_skips.item_penilaian.item_9')
+    </div>
+    <div class="tab-pane fade" id="item-10" role="tabpanel" aria-labelledby="item-10-tab">
+        @include('skips.borang_skips.item_penilaian.item_10')
+    </div>
+</div>
