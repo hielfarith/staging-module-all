ae5a24afe67df080df2df8e8fabe9cd3a3225c2e
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-19 12:31:46 +0800

    [SKIPS] Butiran Pemeriksa

---
+++ resources/views/skips/borang_skips/butiran_pemeriksaan.blade.php
@@ -0,0 +1,43 @@
+<div class="row">
+    <div class="col-md-6 mb-1">
+        <label class="form-label fw-bold">Tarikh Pemeriksaan & Masa Lawatan
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="fp-date-time" class="form-control flatpickr-date-time" placeholder="YYYY-MM-DD HH:MM" />
+    </div>
+
+    <div class="col-md-6 mb-1">
+        <label class="form-label fw-bold text-titlecase">No Pasukan Pemeriksa
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bold text-titlecase">Ketua Pemeriksa
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bold text-titlecase"> Pemeriksa 1
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bold text-titlecase"> Pemeriksa 2
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bold text-titlecase"> Pemeriksa 3
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+</div>
