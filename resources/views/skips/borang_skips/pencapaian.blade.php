e570072eb2a3f657c00fde6da4f27a269bb639e6
seqinah <110380408+seqinah@users.noreply.github.com>
2024-01-20 10:34:44 +0800

    [SKIPS] Ulasan dan pencapaian

---
+++ resources/views/skips/borang_skips/pencapaian.blade.php
@@ -0,0 +1,101 @@
+<div class="row">
+    <div class="col-md-12 mb-1">
+        <label class="form-label fw-bold text-titlecase">Nama Institusi
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" id="" class="form-control">
+    </div>
+
+    <div class="col-md-12 mb-1">
+        <label class="fw-bold form-label">Alamat
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" class="form-control" id="">
+    </div>
+
+    <div class="col-md-4 mb-1">
+        <label class="fw-bold form-label">Negeri
+            <span class="text-danger">*</span>
+        </label>
+        <select class="form-control select2" name="" id="">
+            <option value="" hidden>Negeri</option>
+                @foreach($negeris as $negeri)
+                    <option value="{{$negeri->name}}">{{$negeri->name}}</option>
+                @endforeach
+        </select>
+    </div>
+
+    <div class="table-responsive mb-2">
+        <table class="table header_uppercase table-bordered table-hovered" id="">
+            <thead>
+                <tr>
+                    <th>ITEM</th>
+                    <th width="5%"> PERATUSAN ITEM </th>
+                    <th width="5%">SKOR</th>
+                    <th width="5%">WAJARAN</th>
+                </tr>
+
+                @php
+                    $item_pencapaians = [
+                        '12.0' => 'ITEM 1: PENUBUHAN & PENDAFTARAN',
+                        '7.0' => 'ITEM 2: PENGURUSAN INSTITUSI',
+                        '12.0' => 'ITEM 3: PENGURUSAN KURIKULUM',
+                        '12.0' => 'ITEM 4: PENGAJARAN DAN PEMBELAJARAN',
+                        '10.0' => 'ITEM 5: PENGURUSAN PENILAIAN / PEPERIKSAAN',
+                        '7.0' => 'ITEM 6: PENGURUSAN DAN PEMBANGUNAN GURU',
+                        '5.0' => 'ITEM 7: DISIPLIN',
+                        '13.0' => 'ITEM 8: PIAWAIAN',
+                        '7.0' => 'ITEM 9: KEBERSIHAN DAN KECERIAAN',
+                        '15.0' => 'ITEM 10: PENGURUSAN PELAJAR ANTARABANGSA',
+                    ]
+                @endphp
+
+                <tbody>
+                    @foreach ($item_pencapaians as $itemKey => $item)
+                        <tr>
+                            <td>{{ $item }}</td>
+                            <td>
+                                <a class="text-success">auto-calculated</a>
+                            </td>
+                            <td>
+                                <a class="text-success">auto-calculated</a>
+                            </td>
+                            <td>{{ $itemKey }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+
+                <tfoot>
+                    <tr class="bg-light-primary">
+                        <td colspan="2"> Jumlah Skor Keseluruhan </td>
+                        <td colspan="2">
+                            <a class="text-success">auto-calculated</a>
+                        </td>
+                    </tr>
+                </tfoot>
+            </thead>
+        </table>
+    </div>
+
+    <hr>
+
+    <div class="col-md-4">
+        &nbsp;
+    </div>
+
+    <div class="col-md-4 mb-1">
+        <div class="card bg-light-primary align-content-middle">
+            <div class="card-header">
+                <h4 class="fw-bolder">Gred Penilaian Keseluruhan</h4>
+            </div>
+
+            <div class="card-body">
+                A - 100%
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        &nbsp;
+    </div>
+</div>
---
+++ resources/views/skips/borang_skips/ulasan.blade.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-md-12 mb-1">
+        <label class="fw-bold form-label">Ulasan Ketua Pasukan Pemeriksa :
+            <span class="text-danger">*</span>
+        </label>
+        <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+
+    <div class="col-md-5">
+        <label class="fw-bold form-label">Disediakan Oleh:
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" class="form-control" name="" id="">
+
+        <input type="text" id="" name="" class="form-control flatpickr-basic mt-1" placeholder="YYYY-MM-DD">
+    </div>
+
+    <div class="col-md-2">
+        &nbsp;
+    </div>
+
+    <div class="col-md-5">
+        <label class="fw-bold form-label">Disemak Oleh:
+            <span class="text-danger">*</span>
+        </label>
+        <input type="text" class="form-control" name="" id="">
+
+        <input type="text" id="" name="" class="form-control flatpickr-basic mt-1" placeholder="YYYY-MM-DD">
+    </div>
+</div>
