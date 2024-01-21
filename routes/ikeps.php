76e2b7784a35812cd9167a4a86264112ea166624
Faiz Roslee <faizroslee.unijaya@gmail.com>
2024-01-22 04:14:41 +0800

    ikeps

--- routes/ikeps.php
+++ routes/ikeps.php
@@ -4,13 +4,12 @@ use App\Http\Controllers\PengurusanIkepsController;
 
 Route::controller(PengurusanIkepsController::class)->group(function () {
     Route::prefix('ikeps')->name('ikeps.')->group(function() {
-        Route::get('','BorangIkepsBaru')->name('ikeps_baru');
+        Route::get('borang-pengisian/{tahun?}','BorangIkepsBaru')->name('ikeps_baru');
         Route::post('store/{tab}', 'store')->name('store');
-        Route::get('ringkasan-pengisian','RingkasanIkeps')->name('ringkasan_ikeps');
+        Route::get('ringkasan-pengisian/{tahun?}','RingkasanIkeps')->name('ringkasan_ikeps');
 
         Route::get('laporan-pengisian','LaporanRingkasanIkeps')->name('laporan_ikeps');
         Route::get('pemantauan-pengisian','PemantauanPengisianIkeps')->name('pemantauan_ikeps');
-        Route::get('dashboard','DashboardIkeps')->name('dashboard_ikeps');
-
+        Route::get('dashboard','DashboardIkeps')->name('dashboard_ikeps');       
     });
 });
