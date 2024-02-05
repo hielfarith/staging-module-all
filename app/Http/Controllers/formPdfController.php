50a0526f98f8f9bd641c6beb29f00d0de4d91c66
danialyusri <danialyusri.unijaya@gmail.com>
2024-02-05 11:23:57 +0800

    Update formPdfController.php
    delete function generatePDF4()

--- app/Http/Controllers/formPdfController.php
+++ app/Http/Controllers/formPdfController.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 
 class formPdfController extends Controller
 {
- 
+
     public function generatePDF()
     {
         //$dataFromDatabase = User::all();
-     
+
 
         $pdf = PDF::loadView('pdf.test');
         // return $pdf->download('test.pdf');
@@ -21,7 +21,7 @@ class formPdfController extends Controller
     public function generatePDF2()
     {
         //$dataFromDatabase = User::all();
-        
+
         //  return view ('pdf.sijil');
          $pdf = PDF::loadView('pdf.sijil');
          return $pdf->stream('sijil.pdf');
@@ -29,17 +29,10 @@ class formPdfController extends Controller
     public function generatePDF3()
     {
         //$dataFromDatabase = User::all();
-    
+
         //  return view ('pdf.borang');
          $pdf = PDF::loadView('pdf.borang');
          return $pdf->stream('borang.pdf');
     }
-    public function generatePDF4()
-    {
-        //$dataFromDatabase = User::all();
     
-        //  return view ('pdf.borang');
-         $pdf = PDF::loadView('pdf.borangInstrumen');
-         return $pdf->stream('borangInstrumen.pdf');
-    }
 }
