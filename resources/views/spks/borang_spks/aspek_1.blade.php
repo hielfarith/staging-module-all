7e7afcdbda0fedd533c38b8f33890b45d7fd2b11
seqinah <110380408+seqinah@users.noreply.github.com>
2024-02-13 16:25:11 +0800

    [SKPS] Added catatan each row

--- resources/views/spks/borang_spks/aspek_1.blade.php
+++ resources/views/spks/borang_spks/aspek_1.blade.php
@@ -13,12 +13,13 @@
         width: 100% !important;
         /* word-wrap: break-word; */
     }
+
 </style>
 
 @php
 $aspeks_1 = [
     [
-        'section' => 'Pengurusan Aktiviti Murid',
+    'section' => 'Pengurusan Aktiviti Murid',
         'subSections' => [
             'Arahan Keselamatan Murid Dari Aspek Pergi Dan Balik Sekolah',
             'Arahan Keselamatan Murid Semasa Pengajaran & Pembelajaran Dan Waktu Rehat',
@@ -28,21 +29,22 @@ $aspeks_1 = [
         ]
     ],
     [
-        'section' => 'Arahan Keselamatan Murid Dari Aspek Pergi Dan Balik Sekolah',
+    'section' => 'Arahan Keselamatan Murid Dari Aspek Pergi Dan Balik Sekolah',
         'subSections' => [
-            'Mempunyai data dan rekod cara murid ke sekolah (Berjalan kaki, Berbasikal, Motosikal, Bas sekolah, Dihantar penjaga, Bot/Perahu,Kereta sendiri, Kereta api)',
+            'Mempunyai data dan rekod cara murid ke sekolah (Berjalan kaki, Berbasikal, Motosikal, Bas sekolah, Dihantar penjaga,
+            Bot/Perahu,Kereta sendiri, Kereta api)',
             'Menyedia dan mempamer tatacara keselamatan pergi dan balik sekolah.',
             'pemeriksaan berkala ke atas kenderaan yang digunakan murid ke sekolah.(Basikal, Motosikal, Kereta)',
-            'Mematuhi prosedur dan peraturan berkaitan keselamatan daripada pihak berkuasa berkenaan. (Jaket keselamatan/topi keledar/lesen memandu/cukai jalan dan lain-lain berkaitan)',
+            'Mematuhi prosedur dan peraturan berkaitan keselamatan daripada pihak berkuasa berkenaan. (Jaket keselamatan/topi
+            keledar/lesen memandu/cukai jalan dan lain-lain berkaitan)',
             'Menetapkan laluan pejalan kaki, laluan dan parkir kenderaan yang digunakan oleh murid.',
             'Menetapkan tempat menurunkan dan mengambil murid yang menggunakan bas dan yang dihantar oleh penjaga.',
             'Kawal selia pengurusan sekolah sewaktu murid datang dan balik dari sekolah.',
             'Ada arahan berkaitan keselamatan murid semasa berada di jeti / stesen bas/ stesen kereta api/ dan lain-lain.',
-
         ]
     ],
     [
-        'section' => 'Arahan Keselamatan Murid Semasa Pengajaran & Pembelajaran Dan Waktu Rehat',
+    'section' => 'Arahan Keselamatan Murid Semasa Pengajaran & Pembelajaran Dan Waktu Rehat',
         'subSections' => [
             'Ada Arahan berkaitan keselamatan murid di bilik darjah, makmal. bengkel, lain-lain tempat amali dan bilik-bilik khas.',
             'Pengawasan guru sebelum, semasa, dan selepas aktiviti dijalankan.',
@@ -51,22 +53,20 @@ $aspeks_1 = [
             'Mematuhi peraturan berpakaian semasa melaksanakan aktiviti.',
             'Ada arahan kepada murid tentang kawasan larangan sewaktu rehat.',
             'Rekod keluar masuk dari kawasan sekolah semasa Pengajaran & Pembelajaran.',
-
         ]
     ],
     [
-        'section' => 'Arahan Keselamatan Murid Semasa Aktiviti Kokurikulum, Sukan Dan Permainan',
+    'section' => 'Arahan Keselamatan Murid Semasa Aktiviti Kokurikulum, Sukan Dan Permainan',
         'subSections' => [
             'Ada arahan berkaitan keselamatan murid sebelum, semasa dan selepas aktiviti kokurikulum, sukan dan permainan.',
             'Pengawasan guru sebelum, semasa dan selepas aktiviti dijalankan.',
             'Memastikan kehadiran murid direkodkan.',
             'Memastikan tatacara penggunaan peralatan dipatuhi.',
             'Mematuhi peraturan berpakaian semasa melaksanakan aktiviti.',
-
         ]
     ],
     [
-        'section' => 'Arahan Keselamatan Murid Semasa Aktiviti Lawatan Dan Perkhemahan',
+    'section' => 'Arahan Keselamatan Murid Semasa Aktiviti Lawatan Dan Perkhemahan',
         'subSections' => [
             'Ada arahan berkaitan keselamatan murid sebelum, semasa dan selepas aktiviti lawatan dan perkhemahan dilaksanakan',
             'Pengawasan guru sebelum, semasa dan selepas aktiviti dijalankan.',
@@ -75,11 +75,10 @@ $aspeks_1 = [
             'Mematuhi peraturan berpakaian semasa melaksanakan aktiviti.',
             'Mematuhi prosedur dan peraturan berkaitan lawatan dan perkhemahan.(Surat kebenaran penjaga/perakuan kesihatan)',
             'Memastikan kenderaan yang digunakan untuk membawa murid selamat digunakan.',
-
         ]
     ],
     [
-        'section' => 'Arahan Keselamatan Murid Di Asrama',
+    'section' => 'Arahan Keselamatan Murid Di Asrama',
         'subSections' => [
             'Mempunyai data dan rekod cara murid pergi dan balik asrama.',
             'Menyedia dan mempamer tatacara keselamatan pergi dan balik asrama.',
@@ -89,10 +88,11 @@ $aspeks_1 = [
             'Kawal selia terhadap semua aktiviti murid di asrama.',
             'Ada peraturan berkaitan dengan larangan-larangan lain di asrama.',
             'Pencahayaan mencukupi dalam kawasan asrama.',
-
         ]
     ],
 ];
+
+$number = 1;
 @endphp
 
 <h5 class="card-title fw-bolder text-uppercase">
@@ -105,6 +105,7 @@ $aspeks_1 = [
     <table class="table header_uppercase table-bordered table-hovered" id="spks_aspek1">
         <thead>
             <tr>
+                <th rowspan="2" width="1%">No.</th>
                 <th rowspan="2">Item</th>
                 <th colspan="5">Skor Sekolah</th>
             </tr>
@@ -118,50 +119,61 @@ $aspeks_1 = [
         </thead>
         <tbody>
             @foreach ($aspeks_1 as $index => $aspek_1)
-                <tr>
-                    <td colspan="5" class="bg-light-primary text-uppercase">
-                        {{ $aspek_1['section'] }}
-                    </td>
-                </tr>
-                @foreach ($aspek_1['subSections'] as $subsection_aspek1)
-                    <?php
+            <tr>
+                <td colspan="6" class="bg-light-primary text-uppercase">
+                    {{ $aspek_1['section'] }}
+                </td>
+            </tr>
+            @foreach ($aspek_1['subSections'] as $subsection_aspek1)
+            <?php
                         $name = $index.'_'.$loop->index;
 
                         $withTB = ($aspek_1['section'] == 'Pengurusan Aktiviti Murid') ||  ($aspek_1['section'] == 'Arahan Keselamatan Murid Semasa Aktiviti Lawatan Dan Perkhemahan') || ($aspek_1['section'] == 'Arahan Keselamatan Murid Di Asrama');
                     ?>
-                    <tr>
-                        <td>{{ $subsection_aspek1 }}</td>
-                        <td>
-                            <div class="d-flex justify-content-center align-items-center">
-                                <input class="form-check-input radio-input-2" type="radio" name="{{ $index }}_{{ $loop->index }}" id="0_{{ $index }}_{{ $loop->index }}" value="0">
-                            </div>
-                        </td>
-                        <td>
-                            <div class="d-flex justify-content-center align-items-center">
-                                <input class="form-check-input radio-input-2" type="radio" name="{{ $index }}_{{ $loop->index }}" id="1_{{ $index }}_{{ $loop->index }}" value="1">
-                            </div>
-                        </td>
-                        <td>
-                            <div class="d-flex justify-content-center align-items-center">
-                                <input class="form-check-input radio-input-2" type="radio" name="{{ $index }}_{{ $loop->index }}" id="2_{{ $index }}_{{ $loop->index }}" value="2">
-                            </div>
-                        </td>
-                        @if($withTB)
-                            <td>
-                                <div class="d-flex justify-content-center align-items-center">
-                                    <input class="form-check-input radio-input-2" type="radio" name="{{ $index }}_{{ $loop->index }}" id="TB_{{ $index }}_{{ $loop->index }}" value="TB">
-                                </div>
-                            </td>
-                        @else
-                            <td class="bg-dark"></td>
-                        @endif
-                    </tr>
-                @endforeach
+            <tr>
+                <td>{{ $number++ }}</td>
+                <td>{{ $subsection_aspek1 }}</td>
+                <td>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <input class="form-check-input radio-input-2" type="radio"
+                            name="{{ $index }}_{{ $loop->index }}" id="0_{{ $index }}_{{ $loop->index }}" value="0">
+                    </div>
+                </td>
+                <td>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <input class="form-check-input radio-input-2" type="radio"
+                            name="{{ $index }}_{{ $loop->index }}" id="1_{{ $index }}_{{ $loop->index }}" value="1">
+                    </div>
+                </td>
+                <td>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <input class="form-check-input radio-input-2" type="radio"
+                            name="{{ $index }}_{{ $loop->index }}" id="2_{{ $index }}_{{ $loop->index }}" value="2">
+                    </div>
+                </td>
+                @if($withTB)
+                <td>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <input class="form-check-input radio-input-2" type="radio"
+                            name="{{ $index }}_{{ $loop->index }}" id="TB_{{ $index }}_{{ $loop->index }}" value="TB">
+                    </div>
+                </td>
+                @else
+                <td class="bg-dark"></td>
+                @endif
+            </tr>
+
+            <tr>
+                <td colspan="6" class="bg-light-success">
+                    <input type="text" class="form-control" placeholder="Catatan">
+                </td>
+            </tr>
+            @endforeach
             @endforeach
         </tbody>
         <tfoot>
             <tr class="bg-light-danger">
-                <td class="text-end">
+                <td colspan="2" class="text-end">
                     Skor
                 </td>
                 <td class="text-center"></td>
@@ -170,10 +182,10 @@ $aspeks_1 = [
                 <td class="text-center"></td>
             </tr>
             <tr class="bg-light-danger">
-                <td class="text-end">
+                <td colspan="2"  class="text-end">
                     Jumlah Skor
                 </td>
-               <td colspan="4" class="text-center"></td>
+                <td colspan="4" class="text-center"></td>
             </tr>
         </tfoot>
     </table>
