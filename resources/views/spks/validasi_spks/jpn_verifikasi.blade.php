de0bfb3a59bc07d2c6fd979b94835f8e189ddfe6
hafizruslan <145441557+hafizruslan@users.noreply.github.com>
2024-03-04 09:27:18 +0530

    add verfikasi changes

--- resources/views/spks/validasi_spks/jpn_verifikasi.blade.php
+++ resources/views/spks/validasi_spks/jpn_verifikasi.blade.php
@@ -26,7 +26,13 @@
 </div>
 
 
+
+<?php
+    $id = Request::segment(3);
+?>
 <hr>
+<form id="jpn_verfikasi">
+<input type="hidden" name="spks_id" value="{{$id}}">
 
 <div class="table-responsive mb-5">
     <table class="table header_uppercase table-bordered table-hovered" style="width:100%" id="catatan_sekolah">
@@ -46,18 +52,54 @@
             
         </tbody>
     </table>
-
+<input type="hidden" name="selesai" value="" id="selesai">
     <div class="d-flex justify-content-end align-items-center mt-1">
-        <button type="button" class="btn btn-primary btn-sm waves-effect waves-float waves-light">Selesai</button>
+        <button type="button" class="btn btn-primary btn-sm waves-effect waves-float waves-light" onclick="formsubmitv('1')">Selesai</button>
         <div style="margin-left: 10px;"></div> <!-- Adjust margin as needed -->
-        <button type="button" class="btn btn-primary btn-sm waves-effect waves-float waves-light">Tidak
+        <button type="button" class="btn btn-primary btn-sm waves-effect waves-float waves-light" onclick="formsubmitv('2')">Tidak
             Selesai</button>
     </div>
 </div>
 
+</form>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
 <script type="text/javascript">
-   
+    var id = <?php echo Request::segment(3); ?>;
+    function formsubmitv(argument) {
+        $('#selesai').val(argument);
+        var formData = new FormData(document.getElementById('jpn_verfikasi'));
+        var error = false;
+
+         $('form#jpn_verfikasi').find('radio, input, checkbox, textarea').each(function() {
+            if(this.required && this.type == 'radio' && !this.checked) {
+                var val = $("input[type='radio'][name="+this.name+"]:checked", '#jpn_verfikasi').val();
+                if (typeof val == 'undefined') {
+                    error = true;
+                }
+            }
+        });
+
+        if (error) {
+            Swal.fire('Error', 'Sila isi ruangan yang diperlukan', 'error');
+            return false;
+        }
+        var APIUrl = "{{ env('APP_VERFIKASI_URL') }}" + 'api/spks/save-verfikasi';
+
+        $.ajax({
+            url: APIUrl,
+            method: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+               if (response.status) {
+                    Swal.fire('Success', 'Berjaya', 'success');
+               }
+            }
+        });
+    }
+
 </script>
+
