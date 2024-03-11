767742329ca841a258abe658ea720b9b8cff0c4d
Afzal Nazrin <afzal08.unijaya@gmail.com>
2024-03-11 10:55:09 +0800

    Update index.blade.php

--- resources/views/skips/borang_skips_sekolah/index.blade.php
+++ resources/views/skips/borang_skips_sekolah/index.blade.php
@@ -14,9 +14,10 @@
     {{-- <input type="hidden" name="butiran_id" id="butiran_id" value="{{ $butiran_id }}"> --}}
     <ul class="nav nav-pills nav-justified" role="tablist">
         <li class="nav-item" role="presentation">
-            <a class="text-uppercase text-wrap nav-link fw-bolder active" id="penilai-kendiri-tab" data-bs-toggle="tab"
-                href="#penilai-kendiri" aria-controls="penilai-kendiri" role="tab" aria-selected="true">
-                BUTIRAN PENILAI KENDIRI
+            <a class="text-uppercase text-wrap nav-link fw-bolder active" id="pendidikan-swasta-tab" data-bs-toggle="tab"
+                href="#pendidikan-swasta" aria-controls="pendidikan-swasta" role="tab" aria-selected="true"
+                onclick="tabclicked('#pendidikan-swasta')">
+                BUTIRAN INSTITUSI PENDIDIKAN SWASTA
             </a>
         </li>
         <li class="nav-item" role="presentation">
@@ -27,30 +28,26 @@
             </a>
         </li>
         <li class="nav-item" role="presentation">
-            <a class="text-uppercase text-wrap nav-link fw-bolder" id="pendidikan-swasta-tab" data-bs-toggle="tab"
-                href="#pendidikan-swasta" aria-controls="pendidikan-swasta" role="tab" aria-selected="true"
-                onclick="tabclicked('#pendidikan-swasta')">
-                BUTIRAN INSTITUSI PENDIDIKAN SWASTA
+            <a class="text-uppercase text-wrap nav-link fw-bolder " id="penilai-kendiri-tab" data-bs-toggle="tab"
+                href="#penilai-kendiri" aria-controls="penilai-kendiri" role="tab" aria-selected="true">
+                BUTIRAN PENILAI KENDIRI
             </a>
         </li>
-
     </ul>
 
     <div class="card">
         <div class="card-body">
             <div class="tab-content">
-
-                <div class="tab-pane active" id="penilai-kendiri" role="tabpanel" aria-labelledby="penilai-kendiri-tab">
-                    @include('skips.borang_skips_sekolah.borang.penilai')
-                </div>
-                <div class="tab-pane fade" id="instrumen-skips" role="tabpanel" aria-labelledby="instrumen-skips-tab">
-                    @include('skips.borang_skips_sekolah.borang.instrumen.index')
-                </div>
-
-                <div class="tab-pane fade" id="pendidikan-swasta" role="tabpanel" aria-labelledby="pendidikan-swasta-tab">
+                <div class="tab-pane active" id="pendidikan-swasta" role="tabpanel" aria-labelledby="pendidikan-swasta-tab">
                     @include('skips.borang_skips_sekolah.borang.institut')
                 </div>
 
+                <div class="tab-pane fade" id="instrumen-skips" role="tabpanel" aria-labelledby="instrumen-skips-tab">
+                    @include('skips.borang_skips_sekolah.borang.instrumen.index')
+                </div>
+                <div class="tab-pane fade" id="penilai-kendiri" role="tabpanel" aria-labelledby="penilai-kendiri-tab">
+                    @include('skips.borang_skips_sekolah.borang.penilai')
+                </div>
             </div>
         </div>
     </div>
