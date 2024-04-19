80f381a3cf1ecb6caa4d28ce243d40c56c10c7f7
Afzal Nazrin <afzal08.unijaya@gmail.com>
2024-04-19 19:43:56 +0800

    add konfigurasi instrumen SKPAK

--- resources/views/instrumen_update/view-profile.blade.php
+++ resources/views/instrumen_update/view-profile.blade.php
@@ -8,7 +8,7 @@
                     <div class="row">
                         <h5 class="mb-2 fw-bold">
                             <span class="badge rounded-pill badge-light-primary">
-                                Maklumat MEDAN DATA TAMBAH / KEMASKINI INSTRUMEN SKPAK
+                                Maklumat MEDAN DATA TAMBAH / KEMASKINI INSTRUMEN SKIPS
                             </span>
                         </h5>
                         <div class="col-md-2 mb-1">
@@ -439,6 +439,223 @@
         @endif
         <!-- ----end skps--- -->
 
+        <!-- ----start skps--- -->
+        @if ($instrumen->type == 'SKPAK')
+            <form id="forminstrumenskpak" novalidate="novalidate">
+                <div class="row">
+                    <h5 class="mb-2 fw-bold">
+                        <span class="badge rounded-pill badge-light-primary">
+                            Maklumat MEDAN DATA TAMBAH / KEMASKINI INSTRUMEN SKPAK
+                        </span>
+                    </h5>
+                    <div class="col-md-2 mb-1">
+                        <label class="fw-bold form-label">Nama Instrumen <span class="text-danger">:</span></label>
+                    </div>
+                    <div class="col-md-4 mb-1">
+                        <input type="text" class="form-control" name="nama_instrumen"
+                            value="{{ $instrumen->nama_instrumen }}" {{ $readonly }} {{ $disabled }}
+                            required
+                            onkeypress="return ((event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || event.charCode == 32) || event.charCode == 8">
+                    </div>
+                    <div class="col-md-2 mb-1">
+                        <label class="fw-bold form-label">Tarikh Kuatkuasa<span class="text-danger">:</span></label>
+                    </div>
+                    <div class="col-md-4 mb-1">
+                        <input type="text" class="form-control flatpickr" name="tarikh_kuatkuasa"
+                            value="{{ $instrumen->tarikh_kuatkuasa }}" {{ $readonly }} {{ $disabled }}
+                            required>
+                    </div>
+                    <div class="col-md-2 mb-1">
+                        <label class="fw-bold form-label">Tujuan Instrumen<span class="text-danger">:</span></label>
+                    </div>
+                    <div class="col-md-4 mb-1">
+                        <input type="text" class="form-control" name="tujuan_instrumen"
+                            value="{{ $instrumen->tujuan_instrumen }}" {{ $readonly }} required
+                            {{ $disabled }}>
+                    </div>
+                    <div class="col-md-2 mb-1">
+                        <label class="fw-bold form-label">Status<span class="text-danger">:</span></label>
+                    </div>
+                    <div class="col-md-4 mb-1">
+                        <select class="form-control select2" name="status" required>
+                            <option value="">Sila Pilih</option>
+                            <option value="1" @if ($instrumen?->status == '1') selected @endif>Active
+                            </option>
+                            <option value="2" @if ($instrumen?->status == '2') selected @endif>InActive
+                            </option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <div class="card ">
+                            <div class="card-header" style="background-color: #F3F2F7">
+                                <h4 class="card-title fw-bolder">Pengisian</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row mt-1">
+                                    <div class="col-md-4 ">
+                                        <label class="fw-bold form-label">Institut <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 ">
+                                        <select class="form-control select2" name="jenis_ips" required
+                                            id="jenis_ips">
+                                            <option value="{{ $instrumen->jenis_ips }}">{{ $instrumen->jenis_ips }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-4 mt-1">
+                                        <label class="fw-bold form-label">Peranan <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 mt-1">
+                                        <select class="form-control select2" name="perakuan_oleh" {{ $disabled }}
+                                            required>
+                                            <option value="">Sila Pilih</option>
+                                            <option value="PENTADBIR"
+                                                @if ($instrumen->perakuan_oleh == 'PENTADBIR') selected @endif>PENTADBIR
+                                            </option>
+                                            <option value="GURU INSTITUSI"
+                                                @if ($instrumen->perakuan_oleh == 'GURU INSTITUSI') selected @endif>GURU
+                                                INSTITUSI</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-12 mt-1">
+                                        <label class="fw-bold form-label">Tarikh <span
+                                                class="text-danger">:</span></label>
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Dari</span>
+                                                <input type="date" class="form-control" id="fromDate">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Hingga</span>
+                                                <input type="date" class="form-control" id="toDate">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <div class="card ">
+                            <div class="card-header" style="background-color: #F3F2F7">
+                                <h4 class="card-title fw-bolder">Validasi</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row mt-1">
+                                    <div class="col-md-4 ">
+                                        <label class="fw-bold form-label">Institut <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 ">
+                                        <select class="form-control select2" name="jenis_ips" required
+                                            id="jenis_ips">
+                                            <option value="{{ $instrumen->jenis_ips }}">{{ $instrumen->jenis_ips }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-4 mt-1">
+                                        <label class="fw-bold form-label">Peranan <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 mt-1">
+                                        <select class="form-control select2" name="perakuan_oleh" {{ $disabled }}
+                                            required>
+                                            <option value="">Sila Pilih</option>
+                                            <option value="PENTADBIR"
+                                                @if ($instrumen->perakuan_oleh == 'PENTADBIR') selected @endif>PENTADBIR
+                                            </option>
+                                            <option value="GURU INSTITUSI"
+                                                @if ($instrumen->perakuan_oleh == 'GURU INSTITUSI') selected @endif>GURU
+                                                INSTITUSI</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-12 mt-1">
+                                        <label class="fw-bold form-label">Tarikh <span
+                                                class="text-danger">:</span></label>
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Dari</span>
+                                                <input type="date" class="form-control" id="fromDate">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Hingga</span>
+                                                <input type="date" class="form-control" id="toDate">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <div class="card ">
+                            <div class="card-header" style="background-color: #F3F2F7">
+                                <h4 class="card-title fw-bolder">Verifikasi</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row mt-1">
+                                    <div class="col-md-4 ">
+                                        <label class="fw-bold form-label">Institut <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 ">
+                                        <select class="form-control select2" name="jenis_ips" required
+                                            id="jenis_ips">
+                                            <option value="{{ $instrumen->jenis_ips }}">{{ $instrumen->jenis_ips }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-4 mt-1">
+                                        <label class="fw-bold form-label">Peranan <span
+                                                class="text-danger">:</span></label>
+                                    </div>
+                                    <div class="col-md-8 mt-1">
+                                        <select class="form-control select2" name="perakuan_oleh" {{ $disabled }}
+                                            required>
+                                            <option value="">Sila Pilih</option>
+                                            <option value="PENTADBIR"
+                                                @if ($instrumen->perakuan_oleh == 'PENTADBIR') selected @endif>PENTADBIR
+                                            </option>
+                                            <option value="GURU INSTITUSI"
+                                                @if ($instrumen->perakuan_oleh == 'GURU INSTITUSI') selected @endif>GURU
+                                                INSTITUSI</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-12 mt-1">
+                                        <label class="fw-bold form-label">Tarikh <span
+                                                class="text-danger">:</span></label>
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Dari</span>
+                                                <input type="date" class="form-control" id="fromDate">
+                                                <span class="input-group-text"
+                                                    style="background-color: #F3F2F7">Hingga</span>
+                                                <input type="date" class="form-control" id="toDate">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @if ($readonly != 'readonly')
+                        <div class="d-flex justify-content-end align-items-center my-1">
+                            <button type="submit" class="btn btn-primary float-right">Hantar</button>
+                        </div>
+                    @endif
+                </div>
+            </form>
+        @endif
+        <!-- ----end skps--- -->
     </div>
 
 
